a17519155cf62e99170f7f8f1897fafd1b22d453
Charan161025 <geethacharan314@gmail.com>
2026-01-30 21:55:28 +0530

    Update login.blade.php

--- polling_system/resources/views/login.blade.php
+++ polling_system/resources/views/login.blade.php
@@ -3,12 +3,10 @@
 @section('content')
 <h2>Login</h2>
 
-<div id="error" style="color:red;"></div>
+<div id="msg" style="color:red;"></div>
 
-<form id="loginForm">
-  @csrf
-  <input type="email" name="email" placeholder="Email" required><br><br>
-  <input type="password" name="password" placeholder="Password" required><br><br>
-  <button type="submit">Login</button>
-</form>
+<input type="email" id="email" placeholder="Email"><br><br>
+<input type="password" id="password" placeholder="Password"><br><br>
+
+<button onclick="login()">Login</button>
 @endsection
