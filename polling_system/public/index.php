0f909a553b55ef952932ec4b0db15df7cbb9a9c5
Charan161025 <geethacharan314@gmail.com>
2026-01-30 21:33:50 +0530

    index.php
    Removed old database connection setup and replaced with app.php include.

--- polling_system/public/index.php
+++ polling_system/public/index.php
@@ -1,25 +1,2 @@
 <?php
-
-require __DIR__ . '/../vendor/autoload.php';
-
-use Illuminate\Database\Capsule\Manager as Capsule;
-
-
-
-$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
-$dotenv->safeLoad();
-
-
-$capsule = new Capsule;
-$capsule->addConnection([
-    'driver' => 'sqlite',
-    'database' => __DIR__ . '/../database/database.sqlite',
-    'prefix' => '',
-]);
-$capsule->setAsGlobal();
-$capsule->bootEloquent();
-
-
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-
-require __DIR__ . '/../routes/web.php';
+require __DIR__ . '/app.php';
