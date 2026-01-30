ce64859469875caa6efcaacd8e932ba89a094863
Charan161025 <geethacharan314@gmail.com>
2026-01-30 21:53:41 +0530

    login

--- polling_system/public/js/app.js
+++ polling_system/public/js/app.js
@@ -1,20 +1,42 @@
 
 function login(){
- $.post('/login',{email:$('#email').val(),password:$('#password').val(),_token:$('meta[name=csrf-token]').attr('content')},
- res=>{ if(res.success) location.href='/polls'; else $('#msg').text(res.error); });
+ $.post('/login',{
+   email:$('#email').val(),
+   password:$('#password').val(),
+   _token:$('meta[name=csrf-token]').attr('content')
+ },
+ res=>{
+   if(res.success) location.href='/polls';
+   else $('#msg').text(res.error);
+ });
 }
-function loadPoll(id){ $.get('/poll/'+id,res=>$('#content').html(res)); }
+
+function loadPoll(id){
+ $.get('/poll/'+id,res=>$('#content').html(res));
+}
+
 function vote(pollId,optionId){
- $.post('/vote',{poll_id:pollId,option_id:optionId,_token:$('meta[name=csrf-token]').attr('content')},
+ $.post('/vote',{
+   poll_id:pollId,
+   option_id:optionId,
+   _token:$('meta[name=csrf-token]').attr('content')
+ },
  res=>$('#message').text(res.error ?? 'Vote submitted'));
 }
+
 setInterval(()=>{
- let id=$('#pollId').val(); if(!id) return;
+ let id=$('#pollId').val();
+ if(!id) return;
  $.get('/results/'+id,data=>{
-  let h=''; data.forEach(r=>h+=`<p>${r.option_text}: ${r.total}</p>`);
+  let h='';
+  data.forEach(r=>h+=`<p>${r.option_text}: ${r.total}</p>`);
   $('#results').html(h);
  });
 },1000);
+
 function release(id){
- $.post('/release',{vote_id:id,_token:$('meta[name=csrf-token]').attr('content')},()=>location.reload());
+ $.post('/release',{
+   vote_id:id,
+   _token:$('meta[name=csrf-token]').attr('content')
+ },()=>location.reload());
 }
