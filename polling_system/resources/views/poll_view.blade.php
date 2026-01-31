1bff933fc518c8e72d0b82a299f4c22afbdea1d8
Charan161025 <geethacharan314@gmail.com>
2026-01-31 18:11:14 +0530

    Refactor poll view for improved structure and styling

--- polling_system/resources/views/poll_view.blade.php
+++ polling_system/resources/views/poll_view.blade.php
@@ -1,12 +1,16 @@
-
-<h3>{{ $poll->question }}</h3>
-<input type="hidden" id="pollId" value="{{ $poll->id }}">
-@foreach($options as $o)
-<button onclick="vote({{ $poll->id }},{{ $o->id }})" class="btn btn-secondary mb-2">
-{{ $o->option_text }}
-</button><br>
-@endforeach
-<div id="message"></div>
-<hr>
-<h5>Live Results</h5>
-<div id="results"></div>
+<div class="container mt-4">
+    <h3>{{ $poll['question'] }}</h3>
+    <input type="hidden" id="pollId" value="{{ $poll['id'] }}">
+    
+    @foreach($options as $o)
+    <button onclick="vote({{ $poll['id'] }}, {{ $o['id'] }})" class="btn btn-secondary mb-2">
+        {{ $o['option_text'] }}
+    </button><br>
+    @endforeach
+    
+    <div id="message" class="alert mt-3" style="display:none;"></div>
+    
+    <hr>
+    <h5>Live Results</h5>
+    <div id="results"></div>
+</div>
