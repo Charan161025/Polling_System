0e39df7a4a4d69392ffa95f5c5ace7b5ddcd4f1a
Charan161025 <geethacharan314@gmail.com>
2026-01-31 18:11:44 +0530

    Update layout.blade.php

--- polling_system/resources/views/layout.blade.php
+++ polling_system/resources/views/layout.blade.php
@@ -2,13 +2,17 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Polling</title>
-<meta name="csrf-token" content="{{ csrf_token() }}">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="dummy-token">
+    <title>Poll Platform</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
-<body class="container mt-4">
-<div id="content">@yield('content')</div>
-<script src="/js/app.js"></script>
+<body>
+    <div id="content">
+        @yield('content')
+    </div>
+    <script src="/js/app.js"></script>
 </body>
 </html>
