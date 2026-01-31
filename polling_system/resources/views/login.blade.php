2a0bbc3f85a6cb55a2485c261583b03ec4804a2e
Charan161025 <geethacharan314@gmail.com>
2026-01-31 19:32:51 +0530

    email-pass

--- polling_system/resources/views/login.blade.php
+++ polling_system/resources/views/login.blade.php
@@ -11,11 +11,11 @@
                     <form id="loginForm" onsubmit="login(); return false;">
                         <div class="mb-3">
                             <label>Email</label>
-                            <input type="email" id="email" class="form-control" value="admin@test.com" required autocomplete="email">
+                            <input type="email" id="email" class="form-control" value="" required autocomplete="email">
                         </div>
                         <div class="mb-3">
                             <label>Password</label>
-                            <input type="password" id="password" class="form-control" value="password" required autocomplete="current-password">
+                            <input type="password" id="password" class="form-control" value="" required autocomplete="current-password">
                         </div>
                         <button type="submit" class="btn btn-primary">Login</button>
                         <div id="msg" class="text-danger mt-2"></div>
